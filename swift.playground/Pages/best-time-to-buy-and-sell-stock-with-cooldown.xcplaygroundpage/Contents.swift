6ff2eb28abc6f1872422b0c6f88405fedc572410
Xu Jingxin <sailxjx@gmail.com>
2016-05-12 14:53:28 +0800

    best-time-to-buy-and-sell-stock-with-cooldown

--- swift.playground/Pages/best-time-to-buy-and-sell-stock-with-cooldown.xcplaygroundpage/Contents.swift
+++ swift.playground/Pages/best-time-to-buy-and-sell-stock-with-cooldown.xcplaygroundpage/Contents.swift
@@ -17,83 +17,36 @@
  Special thanks to @dietpepsi for adding this problem and creating all test cases.
 */
 
+// Reference: https://leetcode.com/discuss/71354/share-my-thinking-process
 class Solution {
   func maxProfit(prices: [Int]) -> Int {
-//    var min = -1  // Never buyed
-//    var profit = 0
-//    var gained = 0
-//    var cooldown = false
-//    let priceCount = prices.count
-//    for i in 0..<priceCount {
-//      // Last two prices
-//      let today = prices[i]
-//      let tomorrow = prices[i + 1]
-//      let dayAfterTomorrow = prices[i + 2]
-//      if min == -1 {  // Try buy
-//        if cooldown {  // Skip cooldown
-//          cooldown = false
-//          continue
-//        }
-//        if dayAfterTomorrow >= today {
-//          min = today  // Buy
-//        } else {
-//
-//        }
-//        if tomorrow >= today && dayAfterTomorrow >= tomorrow {
-//          min = today  // Buy
-//        } else if tomorrow >= today && dayAfterTomorrow < tomorrow {
-//
-//        }
-//      } else {  // Try sell
-//        cooldown = true
-//        min = -1
-//      }
-//      if tomorrow > today && dayAfterTomorrow > tomorrow && today < min && cooldown == false {
-//        min = today
-//      }
-//      if cur < min && colldown == false {  // Buy
-//        min = cur
-//      }
-//    }
-//    for price in prices {
-//      if price < min {
-//        min = price
-//      } else {
-//        let diff = price - min
-//        profit = diff > profit ? diff: profit
-//      }
-//    }
-//    return profit
-    return 0
-  }
-
-  func maxProfitWithoutCooldown(prices: [Int]) -> Int {
-    var min = -1
-    var profit = 0
-    let count = prices.count
-    for i in 0..<count {
-      let today = prices[i]
-      if min == -1 {  // Try buy
-        if count - i > 1 {  // Exist tomorrow
-          if prices[i + 1] > today {
-            min = today  // Buy
-          }
-        }
-      } else {  // Try sell
-        if count - 1 > 1 {  // Exist tomorrow
-          if prices[i + 1] < today {
-            profit = today - min  // Sell
-            min = -1
-          }
-        }
-      }
+    var sell = 0
+    var preSell = 0
+    var buy = Int.min
+    var preBuy = Int.min
+    for price in prices {
+      preBuy = buy
+      buy = max(preSell - price, preBuy)
+      preSell = sell
+      sell = max(preBuy + price, preSell)
     }
-    return profit
+    return sell
   }
 }
 
-//Solution().maxProfit([1,2,3,0,2]) == 3
-//Solution().maxProfit([1,9,10,20]) == 19
-//Solution().maxProfit([10,30,1,10]) == 20
-//Solution().maxProfit([1,2,3,2,5,8,4,20]) == 20
-//Solution().maxProfit([10,5,3,20,11,25]) == 22
+Solution().maxProfit([3,1,2]) == 1
+Solution().maxProfit([2,6,8,7,8,7,9,4,1,2,4,5,8]) == 15
+Solution().maxProfit([6,1,6,4,3,0,2]) == 7
+Solution().maxProfit([1,2,4,2,5,7,2,4,9,0,4]) == 11
+Solution().maxProfit([1,2,4,2,5,7,2,4,9,0,9]) == 15
+Solution().maxProfit([6,1,3,2,4,7]) == 6
+Solution().maxProfit([0,1,3,2,4,7]) == 7
+Solution().maxProfit([1,2,3,0,2]) == 3
+Solution().maxProfit([1,2,3,0]) == 2
+Solution().maxProfit([1,9,10,20]) == 19
+Solution().maxProfit([10,30,1,10]) == 20
+Solution().maxProfit([1,2,3,2,5,8,4,20]) == 20
+Solution().maxProfit([10,5,3,20,11,25]) == 22
+Solution().maxProfit([1,4,9,0,0,3,0,0,3]) == 14
+Solution().maxProfit([8,7,6,5,4,3,2,1]) == 0
+Solution().maxProfit([3,7,6,5,4,3,2,1]) == 4
