f7a0dba25b52344a66561a9247dd14cc8ce45e23
Xu Jingxin <sailxjx@gmail.com>
2016-05-12 10:25:50 +0800

    best-time-to-buy-and-sell-stock

---
+++ swift.playground/Pages/best-time-to-buy-and-sell-stock-with-cooldown.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+/*
+ Say you have an array for which the ith element is the price of a given stock on day i.
+
+ Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+ - You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ - After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+ Example:
+
+ ```
+ prices = [1, 2, 3, 0, 2]
+ maxProfit = 3
+ transactions = [buy, sell, cooldown, buy, sell]
+ ```
+
+ Credits:
+ Special thanks to @dietpepsi for adding this problem and creating all test cases.
+*/
+
+class Solution {
+  func maxProfit(prices: [Int]) -> Int {
+    return 0
+  }
+
+  func maxProfitWithoutCooldown(prices: [Int]) -> Int {
+    return 3
+  }
+}
+
+Solution().maxProfit([1,2,3,0,2]) == 3
+Solution().maxProfit([1,2,3,2,5,8,4,20]) == 20
+Solution().maxProfit([10,5,3,20,11,25]) == 22
---
+++ swift.playground/Pages/best-time-to-buy-and-sell-stock.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+/*
+ Say you have an array for which the ith element is the price of a given stock on day i.
+
+ If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+*/
+
+class Solution {
+  func maxProfit(prices: [Int]) -> Int {
+    var min = Int.max
+    var profit = 0
+    for price in prices {
+      if price < min {
+        min = price
+      } else {
+        let diff = price - min
+        profit = diff > profit ? diff: profit
+      }
+    }
+    return profit
+  }
+}
+
+Solution().maxProfit([1,2,3,2,5,8,4,20]) == 19
+Solution().maxProfit([10,5,3,20,11,25]) == 22
+Solution().maxProfit([1,9,10,20]) == 19
+Solution().maxProfit([10,30,1,10]) == 20
